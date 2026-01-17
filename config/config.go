074b67660b3e96a6439a09046db7c243347c1c6a
kida <107628916+kida21@users.noreply.github.com>
2026-01-17 10:52:21 +0300

    config added

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DBHost string
+	DBPort string
+	DBUser string
+	DBPass string
+	DBName string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		DBHost: os.Getenv("DB_HOST"),
+		DBPort: os.Getenv("DB_PORT"),
+		DBUser: os.Getenv("DB_USER"),
+		DBPass: os.Getenv("DB_PASS"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+}
+
+func (c *Config) DSN() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		c.DBHost, c.DBUser, c.DBPass, c.DBName, c.DBPort)
+}

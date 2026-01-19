b683c0fca7c535118ee2083cc2961c5d4194970f
kida <107628916+kida21@users.noreply.github.com>
2026-01-19 10:58:23 +0300

    main updated

--- cmd/main.go
+++ cmd/main.go
@@ -4,36 +4,47 @@ import (
 	"log"
 	"os"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	"github.com/kida21/telegram-langbot/config"
+	"github.com/kida21/telegram-langbot/db"
+	"github.com/kida21/telegram-langbot/internal/bot"
+	"github.com/kida21/telegram-langbot/internal/handlers"
+	"github.com/kida21/telegram-langbot/internal/repositories"
+	"github.com/kida21/telegram-langbot/internal/services"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
+	
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on system environment variables")
+	}
 
-    token := os.Getenv("TELEGRAM_BOT_TOKEN")
-	log.Printf("token:%s",token)
-    bot, err := tgbotapi.NewBotAPI(token)
-    if err != nil {
-        log.Printf("error occured : %v",err)
-    }
+    cfg := config.LoadConfig()
+    db.InitDatabase(cfg)
 
-    bot.Debug = true
-    log.Printf("Authorized on account %s", bot.Self.UserName)
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	api, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
 
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
+	
+	userRepo := repositories.NewUserRepository(db.DB)
+	vocabRepo := repositories.NewVocabularyRepository(db.DB)
+	quizRepo := repositories.NewQuizRepository(db.DB)
+	progressRepo := repositories.NewProgressRepository(db.DB)
 
-    updates := bot.GetUpdatesChan(u)
+	
+	userService := services.NewUserService(userRepo)
+	vocabService := services.NewVocabularyService(vocabRepo)
+	quizService := services.NewQuizService(quizRepo)
+	progressService := services.NewProgressService(progressRepo)
 
-    for update := range updates {
-        if update.Message != nil {
-            log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello, welcome to LanguageBot!")
-            bot.Send(msg)
-        }
-    }
+	
+	handler := handlers.NewHandler(userService, vocabService, quizService, progressService)
+
+	
+	b := bot.NewBot(api, handler)
+	b.Start()
 }

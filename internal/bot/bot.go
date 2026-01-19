086482818848f9a097818de2233dcd861f11e76c
kida <107628916+kida21@users.noreply.github.com>
2026-01-19 10:54:47 +0300

    bot bootstapping impl

---
+++ internal/bot/bot.go
@@ -0,0 +1,33 @@
+package bot
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/kida21/telegram-langbot/internal/handlers"
+)
+
+type Bot struct {
+	api     *tgbotapi.BotAPI
+	handler *handlers.Handler
+}
+
+
+func NewBot(api *tgbotapi.BotAPI, handler *handlers.Handler) *Bot {
+	return &Bot{api: api, handler: handler}
+}
+
+func (b *Bot) Start() {
+	log.Printf("Authorized on account %s", b.api.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := b.api.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			b.handler.HandleUpdate(b.api, update)
+		}
+	}
+}
